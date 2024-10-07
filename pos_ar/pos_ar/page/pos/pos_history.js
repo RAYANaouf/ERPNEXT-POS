e843b3ebd5e585d67348f7713988cbf19d8a0ddb
Rayan Aouf <rayanaouf1512@gmail.com>
2024-10-07 23:22:06 +0100

    progressing

--- pos_ar/pos_ar/page/pos/pos_history.js
+++ pos_ar/pos_ar/page/pos/pos_history.js
@@ -2,8 +2,21 @@
 
 pos_ar.PointOfSale.pos_history = class {
 
-	constructor(){
+	constructor(
+		wrapper
+	){
 		console.log("im pos history")
+	}
+
+
+
+	start_work(){
+		this.prepare_selected_item_cart();
+	}
+
+	prepare_selected_item_cart(){
+		this.wrapper.append('<div id="historyContainer"></div>')
+		this.container = this.wrapper.find('#historyContainer')
 	}
 
 }
