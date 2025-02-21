022a7b3df225111fa799dc486069c7a760da9755
rayan <rayanaouf1512@gmail.com>
2025-02-21 11:18:39 +0100

    progressing good...

--- pos_ar/pos_ar/doctype/pos_info/pos_info.py
+++ pos_ar/pos_ar/doctype/pos_info/pos_info.py
@@ -457,7 +457,7 @@ def get_saled_item(company = None):
             pos_items = frappe.get_all("POS Invoice Item", filters=filters_item , fields=["item_code","qty","rate","brand"])
 
             for item in pos_items:
-                if item["brand"] != 19:
+                if str(item["brand"]) != "19":
                     continue
                 if item["item_code"] in item_sold:
                     item_sold[item["item_code"]]["qty"] += item["qty"]
