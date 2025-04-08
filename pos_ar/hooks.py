a0f071af3fc1a471d514ac0ff6f37e8739db3a85
rayan <rayanaouf1512@gmail.com>
2025-04-08 16:26:31 +0100

    export purchase Order Client Script

--- pos_ar/hooks.py
+++ pos_ar/hooks.py
@@ -256,7 +256,7 @@ fixtures = [
     },
     {
         'dt': 'Client Script',
-        'filters': [['dt', 'in', ['POS Closing Entry','CTN-BOX','Sales Invoice']]]
+        'filters': [['dt', 'in', ['POS Closing Entry','CTN-BOX','Sales Invoice','Purchase Order']]]
     }
 ]
 
