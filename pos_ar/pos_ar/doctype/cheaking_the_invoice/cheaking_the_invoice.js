8f8caf1f19ca73648274f275f1bb2f4a62c3a8d3
Farid Neggaz <faridneggaz3@gmail.com>
2026-01-31 19:30:11 +0100

    add doctype checking the invoice

--- pos_ar/pos_ar/doctype/cheaking_the_invoice/cheaking_the_invoice.js
+++ pos_ar/pos_ar/doctype/cheaking_the_invoice/cheaking_the_invoice.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, rayan aouf and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ajustement Quantite", {
+// frappe.ui.form.on("cheaking the invoice", {
 // 	refresh(frm) {
 
 // 	},
