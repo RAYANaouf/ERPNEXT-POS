7ddeb3b7fbe9fb625b3355b5af3c185e1e046f57
Farid Neggaz <faridneggaz3@gmail.com>
2026-04-28 22:54:41 +0100

    add doctype costumer review  with fixtures

---
+++ pos_ar/pos_ar/doctype/customer_review/test_customer_review.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, rayan aouf and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomerReview(FrappeTestCase):
+	pass
